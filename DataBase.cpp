8321949db1db81ae106f0715076858ff96c64728
Loi-Tap-Lam-Dev <99952931+Loi-Tap-Lam-Dev@users.noreply.github.com>
2022-04-18 22:55:48 +0700

    Update 22

--- DataBase.cpp
+++ DataBase.cpp
@@ -2,96 +2,6 @@
 #include "lib.h"
 
 
-//Database to store longtime data, cant be reset only updated
-struct Year {
-
-    struct Class {
-
-            struct SV_List {
-
-                    int no , idStudent , socialID;
-
-                    string lastName , firstName , gender , dateOfBirth;
-
-                    SV_List* svNext = nullptr;
-            };
-        
-             SV_List* yearClassSV_ListHead = nullptr; // To Save The Head Of The SV List of Class
-
-             string nameClass;
-
-             Class* clNext = nullptr;   
-
-    };
-
-     Class* yearCLassHead = nullptr; // To Save the  = nullptr of the Class List of Year
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-     
-     struct Semester {
-
-             int Term;
-
-             struct SV_List {
-
-                     int no , idStudent , GPA , averageMark;
-
-                     SV_List* svNext = nullptr;
-                
-             };
-
-             SV_List* yearSemesterSv_ListHead = nullptr; // To Save the Head of the SV List of the Semester
-
-             struct Subject {
-
-                     string name_Subject, id_Subject;
-                     string startDate, endDate;
-                
-                     int maximumRegrister;
-
-                     struct Class {
-
-                             struct SV_List {
-
-                                int no , idStudent , midTermMark, finalTermMark , otherMark, totalMark;
-
-                                SV_List* svNext = nullptr;
-
-                             };
-
-                             SV_List* yearSemesterSubjectClassSV_ListHead = nullptr; // To Save the Head of the SV List of Class
-
-                             string nameClass;
-
-                             Class* clNext = nullptr;
-
-                     };
-
-                     Class* yearSemesterSubjectClassHead = nullptr; // To Save the Head of the Class List of Subject
-                
-                     Subject* subNext = nullptr;
-
-             };
-
-             Subject* yearSemesterSubjectHead = nullptr;  // To Save the Head of the Subject List of Semester
-
-             struct Schedule {
-
-             };
-
-
-             Semester* sNext = nullptr;
-
-     };
-
-     Semester* yearSemesterHead = nullptr; // To Save the Head of the Semester List of Year
-
-     string nameYear;
-
-     Year* yNext = nullptr;
-
-};
-
 //Account
 struct Account
 {
@@ -136,8 +46,9 @@ struct School_Year {
 
              struct Student_listMark {
 
-                     int no , idStudent , totalCredit, GPA , averageMark;
-
+                     int no , idStudent , totalCredit = 0, GPA = 0, averageMark = 0;
+                     string fullName;
+                     int count_Course_Attempt = 0; //Dont Print this out (Dung in dong nay)
                      Student_listMark* Prev = nullptr;
                      Student_listMark* Next = nullptr;
                 
