cdd5a021426a4263d13df4598b2d82e901f6047c
WokCao <henryquoccao@gmail.com>
2022-04-22 17:25:28 +0700

    fix 5

--- Print_Data_New.cpp
+++ Print_Data_New.cpp
@@ -45,11 +45,11 @@ void PrintData(School_Year*yearHead)
         for(School_Year::Semester*year_semester_cur=yearHead->yearSemesterHead;year_semester_cur!=nullptr;year_semester_cur=year_semester_cur->Next)
         {
             PrintYearSemester<<"Semester info"+tmp+"Term"+tmp+" Start Date"+tmp+" End Date"+tmp<<endl;
-            PrintYearSemester<<to_string(year_semester_cur->Term)+tmp+year_semester_cur->start_Date+tmp+year_semester_cur->end_Date+tmp<<endl;
+            PrintYearSemester<<year_semester_cur->Term<<tmp+year_semester_cur->start_Date+tmp+year_semester_cur->end_Date+tmp<<endl;
             PrintYearSemester<<"Sv_Semester_Mark"+tmp+"No"+tmp+"Id"+tmp+"Fullname"+tmp+"Total Credit"+tmp+"GPA"+tmp+"Average Mark"+tmp<<endl;
             for(School_Year::Semester::Student_listMark*year_semester_stu_cur=year_semester_cur->yearSemesterStudent_listMarkHead;year_semester_stu_cur!=nullptr;year_semester_stu_cur=year_semester_stu_cur->Next)
             {
-                PrintYearSemester<<year_semester_stu_cur->no<<tmp<<year_semester_stu_cur->idStudent<<tmp+year_semester_stu_cur->fullName+tmp<<year_semester_stu_cur->totalCredit<<tmp+to_string(year_semester_stu_cur->GPA)+tmp+to_string(year_semester_stu_cur->averageMark)+tmp<<endl;
+                PrintYearSemester<<year_semester_stu_cur->no<<tmp<<year_semester_stu_cur->idStudent<<tmp+year_semester_stu_cur->fullName+tmp<<year_semester_stu_cur->totalCredit<<tmp<<year_semester_stu_cur->GPA<<tmp<<year_semester_stu_cur->averageMark<<tmp<<endl;
             }
             PrintYearSemester<<"Subject"+tmp+"NameSubject"+tmp+" IDSubject"+tmp+" StartDate"+tmp+" EndDate"+tmp+" TeacherName"+tmp+" Day_of_session_1"+tmp+" Time_Day_1"+tmp+" Day_of_session_2"+tmp+" Time_Day_2"+tmp+" Number_Credit"+tmp+"MaxRegister"+tmp<<endl;
             for(School_Year::Semester::Subject*year_semester_sub_cur=year_semester_cur->yearSemesterSubjectHead;year_semester_sub_cur!=nullptr;year_semester_sub_cur=year_semester_sub_cur->Next)
@@ -59,9 +59,9 @@ void PrintData(School_Year*yearHead)
                 for(School_Year::Semester::Subject::Student_listMark*year_semester_sub_stu_cur=year_semester_sub_cur->yearSemesterSubStudent_ListHead;year_semester_sub_stu_cur!=nullptr;year_semester_sub_stu_cur=year_semester_sub_stu_cur->Next)
                 {
                     if(year_cur->Next==nullptr)
-                    PrintYearSemester<<year_semester_sub_stu_cur->no<<tmp<<year_semester_sub_stu_cur->idStudent<<tmp+year_semester_sub_stu_cur->FullName+tmp+to_string(year_semester_sub_stu_cur->midTermMark)+tmp+to_string(year_semester_sub_stu_cur->finalTermMark)+tmp+to_string(year_semester_sub_stu_cur->otherMark)+tmp+to_string(year_semester_sub_stu_cur->totalMark)+tmp;
+                    PrintYearSemester<<year_semester_sub_stu_cur->no<<tmp<<year_semester_sub_stu_cur->idStudent<<tmp+year_semester_sub_stu_cur->FullName+tmp<<year_semester_sub_stu_cur->midTermMark<<tmp<<year_semester_sub_stu_cur->finalTermMark<<tmp<<year_semester_sub_stu_cur->otherMark<<tmp<<year_semester_sub_stu_cur->totalMark<<tmp;
                     else
-                    PrintYearSemester<<year_semester_sub_stu_cur->no<<tmp<<year_semester_sub_stu_cur->idStudent<<tmp+year_semester_sub_stu_cur->FullName+tmp+to_string(year_semester_sub_stu_cur->midTermMark)+tmp+to_string(year_semester_sub_stu_cur->finalTermMark)+tmp+to_string(year_semester_sub_stu_cur->otherMark)+tmp+to_string(year_semester_sub_stu_cur->totalMark)+tmp<<endl;
+                    PrintYearSemester<<year_semester_sub_stu_cur->no<<tmp<<year_semester_sub_stu_cur->idStudent<<tmp+year_semester_sub_stu_cur->FullName+tmp<<year_semester_sub_stu_cur->midTermMark<<tmp<<year_semester_sub_stu_cur->finalTermMark<<tmp<<year_semester_sub_stu_cur->otherMark<<tmp<<year_semester_sub_stu_cur->totalMark<<tmp<<endl;
                 }
             }
         }
