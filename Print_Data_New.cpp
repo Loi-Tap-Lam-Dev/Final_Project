6aed8700aa8e2f9c00088f6116a5b6993181f583
WokCao <henryquoccao@gmail.com>
2022-04-22 16:40:13 +0700

    fix 3

--- Print_Data_New.cpp
+++ Print_Data_New.cpp
@@ -29,9 +29,9 @@ void PrintData(School_Year*yearHead)
             for(School_Year::Year_Class::SV_List*year_class_sv_cur=year_class_cur->yearClassSV_ListHead;year_class_sv_cur!=nullptr;year_class_sv_cur=year_class_sv_cur->Next)
             {
                 if(year_cur->Next==nullptr)
-                PrintYearClass<<to_string(year_class_sv_cur->no)+tmp+to_string(year_class_sv_cur->idStudent)+tmp+to_string(year_class_sv_cur->socialID)+tmp+year_class_sv_cur->firstName+tmp+year_class_sv_cur->lastName+tmp+year_class_sv_cur->gender+tmp+year_class_sv_cur->dateOfBirth;
+                PrintYearClass<<year_class_sv_cur->no<<tmp<<year_class_sv_cur->idStudent<<tmp<<year_class_sv_cur->socialID<<tmp+year_class_sv_cur->firstName+tmp+year_class_sv_cur->lastName+tmp+year_class_sv_cur->gender+tmp+year_class_sv_cur->dateOfBirth;
                 else
-                PrintYearClass<<to_string(year_class_sv_cur->no)+tmp+to_string(year_class_sv_cur->idStudent)+tmp+to_string(year_class_sv_cur->socialID)+tmp+year_class_sv_cur->firstName+tmp+year_class_sv_cur->lastName+tmp+year_class_sv_cur->gender+tmp+year_class_sv_cur->dateOfBirth<<endl;
+                PrintYearClass<<year_class_sv_cur->no<<tmp<<year_class_sv_cur->idStudent<<tmp<<year_class_sv_cur->socialID<<tmp+year_class_sv_cur->firstName+tmp+year_class_sv_cur->lastName+tmp+year_class_sv_cur->gender+tmp+year_class_sv_cur->dateOfBirth<<endl;
             }
         }
     }
@@ -49,19 +49,19 @@ void PrintData(School_Year*yearHead)
             PrintYearSemester<<"Sv_Semester_Mark"+tmp+"No"+tmp+"Id"+tmp+"Fullname"+tmp+"Total Credit"+tmp+"GPA"+tmp+"Average Mark"+tmp<<endl;
             for(School_Year::Semester::Student_listMark*year_semester_stu_cur=year_semester_cur->yearSemesterStudent_listMarkHead;year_semester_stu_cur!=nullptr;year_semester_stu_cur=year_semester_stu_cur->Next)
             {
-                PrintYearSemester<<to_string(year_semester_stu_cur->no)+tmp+to_string(year_semester_stu_cur->idStudent)+tmp+year_semester_stu_cur->fullName+tmp+to_string(year_semester_stu_cur->totalCredit)+tmp+to_string(year_semester_stu_cur->GPA)+tmp+to_string(year_semester_stu_cur->averageMark)+tmp<<endl;
+                PrintYearSemester<<year_semester_stu_cur->no<<tmp<<year_semester_stu_cur->idStudent<<tmp+year_semester_stu_cur->fullName+tmp<<year_semester_stu_cur->totalCredit<<tmp+to_string(year_semester_stu_cur->GPA)+tmp+to_string(year_semester_stu_cur->averageMark)+tmp<<endl;
             }
             PrintYearSemester<<"Subject"+tmp+"NameSubject"+tmp+" IDSubject"+tmp+" StartDate"+tmp+" EndDate"+tmp+" TeacherName"+tmp+" Day_of_session_1"+tmp+" Time_Day_1"+tmp+" Day_of_session_2"+tmp+" Time_Day_2"+tmp+" Number_Credit"+tmp+"MaxRegister"+tmp<<endl;
             for(School_Year::Semester::Subject*year_semester_sub_cur=year_semester_cur->yearSemesterSubjectHead;year_semester_sub_cur!=nullptr;year_semester_sub_cur=year_semester_sub_cur->Next)
             {
-                PrintYearSemester<<year_semester_sub_cur->name_Subject+tmp+year_semester_sub_cur->id_Subject+tmp+year_semester_sub_cur->startDate+tmp+year_semester_sub_cur->endDate+tmp+year_semester_sub_cur->teacher_Name+tmp+year_semester_sub_cur->day_Of_Session_1+tmp+year_semester_sub_cur->at_Time_1+tmp+year_semester_sub_cur->day_Of_Session_2+tmp+year_semester_sub_cur->at_Time_2+tmp+to_string(year_semester_sub_cur->number_Of_Credit)+tmp+to_string(year_semester_sub_cur->maximumRegrister)+tmp<<endl;
+                PrintYearSemester<<year_semester_sub_cur->name_Subject+tmp+year_semester_sub_cur->id_Subject+tmp+year_semester_sub_cur->startDate+tmp+year_semester_sub_cur->endDate+tmp+year_semester_sub_cur->teacher_Name+tmp+year_semester_sub_cur->day_Of_Session_1+tmp+year_semester_sub_cur->at_Time_1+tmp+year_semester_sub_cur->day_Of_Session_2+tmp+year_semester_sub_cur->at_Time_2+tmp<<year_semester_sub_cur->number_Of_Credit<<tmp<<year_semester_sub_cur->maximumRegrister<<tmp<<endl;
                 PrintYearSemester<<"Subject_Mark"+tmp+"no"+tmp+"idStudent"+tmp+"Fullname"+tmp+"midTerm"+tmp+"finalTerm"+tmp+"other"+tmp+"total"+tmp<<endl;
                 for(School_Year::Semester::Subject::Student_listMark*year_semester_sub_stu_cur=year_semester_sub_cur->yearSemesterSubStudent_ListHead;year_semester_sub_stu_cur!=nullptr;year_semester_sub_stu_cur=year_semester_sub_stu_cur->Next)
                 {
                     if(year_cur->Next==nullptr)
-                    PrintYearSemester<<to_string(year_semester_sub_stu_cur->no)+tmp+to_string(year_semester_sub_stu_cur->idStudent)+tmp+year_semester_sub_stu_cur->FullName+tmp+to_string(year_semester_sub_stu_cur->midTermMark)+tmp+to_string(year_semester_sub_stu_cur->finalTermMark)+tmp+to_string(year_semester_sub_stu_cur->otherMark)+tmp+to_string(year_semester_sub_stu_cur->totalMark)+tmp;
+                    PrintYearSemester<<year_semester_sub_stu_cur->no<<tmp<<year_semester_sub_stu_cur->idStudent<<tmp+year_semester_sub_stu_cur->FullName+tmp+to_string(year_semester_sub_stu_cur->midTermMark)+tmp+to_string(year_semester_sub_stu_cur->finalTermMark)+tmp+to_string(year_semester_sub_stu_cur->otherMark)+tmp+to_string(year_semester_sub_stu_cur->totalMark)+tmp;
                     else
-                    PrintYearSemester<<to_string(year_semester_sub_stu_cur->no)+tmp+to_string(year_semester_sub_stu_cur->idStudent)+tmp+year_semester_sub_stu_cur->FullName+tmp+to_string(year_semester_sub_stu_cur->midTermMark)+tmp+to_string(year_semester_sub_stu_cur->finalTermMark)+tmp+to_string(year_semester_sub_stu_cur->otherMark)+tmp+to_string(year_semester_sub_stu_cur->totalMark)+tmp<<endl;
+                    PrintYearSemester<<year_semester_sub_stu_cur->no<<tmp<<year_semester_sub_stu_cur->idStudent<<tmp+year_semester_sub_stu_cur->FullName+tmp+to_string(year_semester_sub_stu_cur->midTermMark)+tmp+to_string(year_semester_sub_stu_cur->finalTermMark)+tmp+to_string(year_semester_sub_stu_cur->otherMark)+tmp+to_string(year_semester_sub_stu_cur->totalMark)+tmp<<endl;
                 }
             }
         }
